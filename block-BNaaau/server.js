c0bf2517c52cdf9c76a1f121d2a262c0cca67510
Ravindra-Shastri <ravindrashastri90@gmail.com>
2021-10-26 00:54:03 +0530

    index.md

--- block-BNaaau/server.js
+++ block-BNaaau/server.js
@@ -85,10 +85,141 @@ var server = http.createServer(handleRequest);
 
 function handleRequest(req, res){
  res.setHeader('content-type', 'text/html')
-res.end(<h3>Welcome to altcampus</h3>);
+ res.end(<h3>Welcome to altcampus</h3>);
 }
 
 server.listen(PORT, () => {
     console.log('server listening on port' + PORT)    
 });
 
+// Q 8 
+
+var http = require('http');
+
+var PORT = 8000;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res){
+ res.writeHead(201,{'content-type': 'text/html'})
+ res.end(<h3>Welcome to altcampus</h3>);
+}
+
+server.listen(PORT, () => {
+    console.log('server listening on port' + PORT)    
+});
+
+// Q 9
+
+var http = require('http');
+
+var PORT = 8888;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res){
+ res.writeHead(201,{'content-type': 'application/json'})
+ res.end(JSON.stringify({
+     success: true,
+     message: 'Welcome to Nodejs'
+ }));
+}
+
+server.listen(PORT, () => {
+    console.log('server listening on port' + PORT)    
+});
+
+
+// Q 10
+
+var http = require('http');
+
+var PORT = 5050;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res){
+    console.log(req.method);
+ res.writeHead(201,{'content-type': 'text/html'})
+ res.end('<h2>posted for first time </h2>');
+}
+
+server.listen(PORT, () => {
+    console.log('server listening on port' + PORT)    
+});
+
+
+// Q 11
+
+var http = require('http');
+
+var PORT = 2345;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res){
+    if(req.method === 'GET' && req.url === '/'){
+        res.setHeader('content-type', 'text/plain');
+        res.end('Ravindra');
+    } else if(req.method === 'GET' && req.url === '/about'){
+        res.setHeader('content-type', 'text/html');
+        res.end('<h2>Ravindra</h2>')
+    }else{
+      res.statusCode = 404;
+      res.end('page Not Found');
+    }
+}
+
+server.listen(PORT, () => {
+    console.log('server listening on port' + PORT)    
+});
+
+
+// Q 12
+
+var http = require('http');
+
+var fs = require('fs');
+
+var PORT = 2345;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res){
+  if(req.method === 'GET' && req.url === './users'){
+      res.setHeader('content-Type', 'text/html');
+      fs.createReadStream('./form.html').pipe(res);
+  } 
+  if(req.method === 'POST' && req.url === '/users'){
+      res.end('Posted for the second time');
+  } 
+}
+
+server.listen(PORT, () => {
+    console.log('server listening on port' + PORT)    
+});
+
+
+// Q 13 
+
+var http = require('http');
+
+var fs = require('fs');
+
+var url = require('url');
+
+var PORT = 2345;
+
+var server = http.createServer(handleRequest);
+
+function handleRequest(req, res){
+   var parsedUrl = url.parse(req.url, true);
+   console.log(parsedUrl.pathname, req.url);
+   console.log(parsedUrl);
+   res.setHeader('content-Type', 'application/json')
+   res.end(JSON.stringify(parsedUrl.query))
+}
+
+server.listen(PORT, () => {
+    console.log('server listening on port' + PORT)    
+});
